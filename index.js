9997ce8954f7946f7efed521d00a66b180d6c8ca
wilhelmine-erber <mine.erdbeer@gmail.com>
2022-04-01 13:45:24 +0200

    solution

--- index.js
+++ index.js
@@ -2,33 +2,28 @@
 const button = document.querySelector('button')
 const ulElement = document.querySelector('ul')
 
-// Create a list of your favourite destinations with a button to toggle the list
-const callbackToggleList = (event) => {
+// button toggle to hide or show the list (ul)
+button.addEventListener('click', () => {
     ulElement.classList.toggle('d-none')
     button.textContent = ulElement.classList.contains('d-none') ? 'Show destinations' : 'Hide destinations'
-}
-
-button.addEventListener('click', callbackToggleList)
+})
 
 // Let the user select the favourite destination
-
 const liElement = document.querySelectorAll('li')
 
-callbackSelect = (event) => {
-    // console.log(event.target)
-    // console.dir(event.target)
-    // change color of clicked element
-    const selected = document.querySelector('.active')
-    selected.classList.remove('active')
-    event.target.classList.add('active')
-
-    let text = document.querySelector('.text')
-    text.classList.add('alert', 'alert-success', 'mt-4', 'message')
-    text.textContent = `You click ${event.target.textContent}`
-}
-
 liElement.forEach( destinaions =>{
-    destinaions.addEventListener('click', callbackSelect)
+
+    destinaions.addEventListener('click', (event) => {
+        // change color of clicked element
+        const selected = document.querySelector('.active')
+        selected.classList.remove('active')
+        event.target.classList.add('active')
+
+        // add text fyi
+        let text = document.querySelector('.text')
+        text.classList.add('alert', 'alert-success', 'mt-4', 'message')
+        text.textContent = `You click ${event.target.textContent}`
+    })
 })
 
 
